fedbc826c744b60729a1d565b3454d3f6ad26a11
hs.ko <tpsxja)!(@>
2025-01-14 17:54:09 +0900

    onpopstate 연동 중인데... 아ㅣ직 진행중

---
+++ js/common.js
@@ -0,0 +1,18 @@
+let pageSection = document.querySelector('body > section');
+
+function getPage(uri) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', '/page' + uri + '.html', false);
+    xhr.onreadystatechange = e => {
+        if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+            history.pushState(pageSection.innerHTML, document.title, uri);
+            pageSection.innerHTML = xhr.responseText;
+
+        }
+    }
+    xhr.send();
+}
+
+window.onpopstate = e => {
+    console.dir(e.state);
+}
