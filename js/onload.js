30bb1a2149fc8a826da311b556dbf61a7a9b1d13
hs.ko <tpsxja)!(@>
2025-01-14 16:00:04 +0900

    add popper library

---
+++ js/onload.js
@@ -0,0 +1,9 @@
+window.onload = e => {
+    /*TIP popper library*/
+    [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]')).map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
+
+
+}
