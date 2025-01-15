305de1151e34bf6ef9f965db9a439a17515221c9
hs.ko <tpsxja)!(@>
2025-01-15 11:32:30 +0900

    - 404 페이지
    TODO LIST
     - 나머지 페이지 연동

--- js/onload.js
+++ js/onload.js
@@ -4,5 +4,6 @@ window.onload = e => {
         return new bootstrap.Tooltip(tooltipTriggerEl)
     })
 
+    const pageSection = document.querySelector('body > section');
     history.replaceState(pageSection.innerHTML, document.title, location.pathname + location.search);
 }
