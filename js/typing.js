e101f58dbae30867dc3a7feff283943733d04915
hs.ko <tpsxja)!(@>
2025-01-16 17:33:26 +0900

    # [테스트 필요] 타이핑 스크립트 - XML -> JSON 오브젝트화  1. (완) 태그 존재여부  2. (완) start tag 읽어 온다.  3. (완) 2의 자료를 오브젝트화 한다.  4. (완) 오브젝트화한 데이터를 저장 후 나머지 String에 대해 처리
    TODO LIST
    - 타이핑 스크립트
    - 콘솔 컴포넌트 아이콘 별 시각효과
    - index page 그래프 시각효과

--- js/typing.js
+++ js/typing.js
@@ -12,10 +12,14 @@ function typing(nodeQueryName, contentXML, intervalMilliSeconds) {
             let tagObject = {name: undefined, attribute: undefined, content: []};
             let tagContents = tag.substring(tag.indexOf('<') + 1, tag.lastIndexOf('>') - 1);
 
-            if (tagContents.indexOf('=') === -1) tagObject.name = tagContents;
+            if (tagContents.indexOf('=') === -1) tagObject.name = tagContents?.trim();
 
-            while (tagContents.indexOf('=') !== -1) {
-
+            if (tagContents.indexOf('=') !== -1) {
+                const tagAttributes = tagContents.split('=');
+                for (let i = 0; i < tagAttributes.length; i = i + 2) {
+                    const name = tagAttributes[i]?.trim();
+                    tagObject.attribute[name] = tagAttributes[i + 1].trim();
+                }
             }
 
             return tagObject;
@@ -24,40 +28,53 @@ function typing(nodeQueryName, contentXML, intervalMilliSeconds) {
         substringPrefixTagContent: function (text) {
             const tagPrefix = text.search(/(<\w+)|(<\w+\/>)/);
             return text.substring(tagPrefix, text.indexOf('>', tagPrefix + 1));
+        },
+
+        convertXmlToJSON: function (text) {
+            let contentArrays = [];
+            let remainString = text;
+
+            //TIP: 다음 정규식에 해당하는 태그가 있는지 테스트한다.
+            const endTagRegExp = /(<\/\w+>)|(<\w+\/>)/;
+            while (endTagRegExp.test(remainString)) {
+                const firstTagContent = util.substringPrefixTagContent(remainString);
+                if (util.isEmpty(firstTagContent)) break;
+
+                const firstTagIndex = remainString.indexOf(firstTagContent);
+
+                // TIP: 찾게된 'firstTagContent' 변수 앞에 다른 텍스트가 있다면
+                if (firstTagIndex !== 0) {
+                    const beforeText = remainString.substring(0, firstTagIndex);
+                    contentArrays.push(beforeText);
+                    remainString = remainString.replace(beforeText, '');
+                }
+
+                let tag = util.parsingTagObject(firstTagContent);
+
+                if (util.isSingleTag(firstTagContent)) {
+                    contentArrays.push(tag);
+                    continue;
+                }
+
+                tag.content = remainString.substring(firstTagContent.length, remainString.indexOf('<' + tag.name + '/>') - 1);
+                contentArrays.push(tag);
+            }
+
+            if (!util.isEmpty(remainString)) contentArrays.push(remainString);
+
+            return contentArrays;
         }
     }
 
     /************************************START**FUNCTION***LINE************************************/
 
-    let contentJson = [];
-    let remainString = contentXML;
-
-    //TIP: 다음 정규식에 해당하는 태그가 있는지 테스트한다.
-    const endTagRegExp = /(<\/\w+>)|(<\w+\/>)/;
-    while (endTagRegExp.test(remainString)) {
-        const firstTagContent = util.substringPrefixTagContent(remainString);
-        if (util.isEmpty(firstTagContent)) break;
-
-        const firstTagIndex = remainString.indexOf(firstTagContent);
-        if (firstTagIndex !== 0) {
-            const prefixText = remainString.substring(firstTagIndex + firstTagContent.length);
-            contentJson.push(prefixText);
-            remainString = remainString.replace(prefixText, '');
-        }
-
-        let firstTag = util.parsingTagObject(firstTagContent);
-
-        if (util.isSingleTag(firstTagContent)) {
-            contentJson.push(firstTag);
-        } else {
 
 
-
-        }
-    }
+    console.log(JSON.stringify(util.convertXmlToJSON(contentXML)));
 
 
     let targetNode = document.querySelector(nodeQueryName);
-
-    console.log(JSON.stringify(contentJson));
 }
+
+
+
