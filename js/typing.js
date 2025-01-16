efbc500106ea6d4e0981a14bb91add7ace5bcccc
hs.ko <tpsxja)!(@>
2025-01-16 16:41:38 +0900

    # 타이핑 스크립트 - XML -> JSON 오브젝트화  1. (완) 태그 존재여부  2. (완) start tag 읽어 온다.  3. (진행중) 2의 자료를 오브젝트화 한다.  4. 오브젝트화한 데이터를 저장 후 나머지 String에 대해 처리
    TODO LIST
    - 타이핑 스크립트
    - 콘솔 컴포넌트 아이콘 별 시각효과
    - index page 그래프 시각효과

--- js/typing.js
+++ js/typing.js
@@ -1,5 +1,5 @@
-function typing(nodeQueryName, contentXML) {
-    let contentJson = {};
+function typing(nodeQueryName, contentXML, intervalMilliSeconds) {
+    let contentJson = [];
 
     let remainString = contentXML;
 
@@ -8,7 +8,7 @@ function typing(nodeQueryName, contentXML) {
     }
 
     function parsingTagObject(tag) {
-        let tagObject = {name: undefined, attribute: undefined, content: undefined};
+        let tagObject = {name: undefined, attribute: undefined, content: []};
         let tagContents = tag.substring(tag.indexOf('<') + 1, tag.lastIndexOf('>') - 1);
 
         if (tagContents.indexOf('=') === -1) tagObject.name = tagContents;
@@ -20,8 +20,20 @@ function typing(nodeQueryName, contentXML) {
         return tagObject;
     }
 
-    while (existedTag(remainString)) {
-        const firstTagContent = findFirstTag(remainString);
+    //TIP: 다음 정규식에 해당하는 태그가 있는지 테스트한다.
+    const endTagRegExp = /(<\/\w+>)|(<\w+\/>)/;
+    while (endTagRegExp.test(remainString)) {
+        const tagPrefix = remainString.search(/(<\w+)|(<\w+\/>)/);
+        const firstTagContent = remainString.substring(tagPrefix, remainString.indexOf('>', tagPrefix + 1));
+        if (firstTagContent === null) break;
+
+        const firstTagIndex = remainString.indexOf(firstTagContent);
+        if (firstTagIndex !== 0) {
+            const prefixText = remainString.substring(firstTagIndex + firstTagContent.length);
+            contentJson.data.push(prefixText);
+            remainString = remainString.replace(prefixText, '');
+        }
+
         let firstTag = parsingTagObject(firstTagContent);
 
 
