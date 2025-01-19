835a66c3109aef07cfa9c680abeba413912a4f0f
Xasquatch <bigfoot7774@gmail.com>
2025-01-19 19:03:11 +0900

    - 모바일용 메뉴 버튼 재디자인 - 모바일용 취소키
    TODO LIST
    - 콘솔 컴포넌트 아이콘 별 시각효과
    - index page 그래프 시각효과

--- js/window.js
+++ js/window.js
@@ -1,6 +1,16 @@
 const indexContentsData = pageSection.innerHTML;
 pageSection.innerHTML = '';
 
+window.addEventListener('keyup', e => {
+    alert(e.key);
+    if (e.key === 'Escape') {
+        const activeNodes = document.querySelectorAll('.active');
+        for (let node of activeNodes) {
+            node.classList.remove('active');
+        }
+    }
+});
+
 window.addEventListener('keydown', e => {
     if (e.key === 'F5') {
         e.preventDefault();
