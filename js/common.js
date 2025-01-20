7a94c9f413757b98e6f195e93cbdf280bcf4a3f5
hs.ko <tpsxja)!(@>
2025-01-20 15:46:07 +0900

    - 더블클릭 탭 클리어타임아웃 최적화 및 설명란 추가
    TODO LIST
    - 콘솔 컴포넌트 아이콘 별 시각효과
    - index page 그래프 시각효과

--- js/common.js
+++ js/common.js
@@ -25,15 +25,26 @@ function getPage(uri) {
     xhr.send();
 }
 
+let timeoutAddress;
+const explainNav = document.querySelector('#explain-nav');
 function doubleClickCheck(object, callback) {
+    if (timeoutAddress !== undefined)
+        clearTimeout(timeoutAddress);
+
+    explainNav.innerHTML = '';
+
     const classList = object?.classList;
     if (classList?.contains('shine')) {
         document.querySelectorAll('.shine')?.forEach(obj => obj?.classList.remove('shine'));
+        timeoutAddress = undefined;
         callback();
     } else {
         document.querySelectorAll('.shine')?.forEach(obj => obj?.classList.remove('shine'));
         classList?.add('shine');
-        setTimeout(() => {
+        typing(explainNav, object?.dataset?.explain, 10);
+        timeoutAddress = setTimeout(() => {
+            explainNav.innerHTML = '';
+            timeoutAddress = undefined;
             if (classList?.contains('shine'))
                 classList?.remove('shine');
         }, 10000);
