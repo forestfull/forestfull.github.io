2775506eb4d6197793969b8b4c7533e31c9b832e
Xasquatch <bigfoot7774@gmail.com>
2025-02-20 19:58:19 +0900

    문서같은 구성이 필요

--- js/paging.js
+++ js/paging.js
@@ -2,32 +2,36 @@ const pageSection = document.getElementById('mainPagingSection');
 const devUriPrefix = location.protocol === 'http:' ? '/forest-full.github.io' : '';
 const isDevelopMode = ['localhost', '127.0.0.1'].includes(location.hostname);
 
+const XML = {
+    parse(xmlText) {
+        let parsedXml = new DOMParser().parseFromString(xmlText, "application/xml");
+        parsedXml.get = (query) => {
+            return parsedXml.querySelector(query);
+        };
+        parsedXml.getAll = (query) => {
+            return parsedXml.querySelectorAll(query);
+        };
+        return parsedXml;
+    }
+}
+
+const decorator = {
+    title(string) {
+        return `<div class="text-center">
+              <h2 class="display-5 my-5 mx-auto fw-bold d-flex justify-content-center">
+                <label class="align-self-center">${string}</label>
+              </h2>
+            </div>`;
+    },
+    explain(string){
+        return `${string}`;
+    },
+    body(string){
+        return `${string}`;
+    },
+}
+
 function getPage(uri) {
-    const XML = {
-        parse(xmlText) {
-            let parsedXml = new DOMParser().parseFromString(xmlText, "application/xml");
-            parsedXml.get = (query) => {
-                return parsedXml.querySelector(query);
-            };
-            parsedXml.getAll = (query) => {
-                return parsedXml.querySelectorAll(query);
-            };
-            return parsedXml;
-        }
-    }
-
-    const decorator = {
-        getTitle(string) {
-            return `<div class="text-center">
-                  <h2 class="display-5 my-5 mx-auto fw-bold d-flex justify-content-center">
-                    <label class="align-self-center">${string}</label>
-                  </h2>
-                </div>`;
-        },
-
-    }
-
-
     document.getElementById('nav-list').classList.remove('active');
 
     if (uri === undefined || uri === devUriPrefix + '/' || uri === devUriPrefix + '/index.html') {
@@ -45,7 +49,9 @@ function getPage(uri) {
 
             const parsedXML = XML.parse(xhr.responseText);
             let templateHTML = '';
-            templateHTML += decorator.getTitle(parsedXML.get('title').innerHTML);
+            templateHTML += decorator.title(parsedXML.get('title').innerHTML);
+            templateHTML += decorator.explain(parsedXML.get('explain').innerHTML);
+            templateHTML += decorator.body(parsedXML.get('body').innerHTML);
 
             typing(pageSection, templateHTML, 10);
         }
