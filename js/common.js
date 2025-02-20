5b506d8a6ddce22d725f0eeb92108b7a84d769a9
Xasquatch <bigfoot7774@gmail.com>
2025-02-20 13:18:49 +0900

    정리: 페이지 소개 부분 다시 짜기로 함
    order 1. 페이지별로 짠다
    order 2. 합친 요약본을 인덱스로 짠다

--- js/common.js
+++ js/common.js
@@ -1,13 +1,9 @@
 const pageSection = document.querySelector('body > section');
 const devUriPrefix = location.protocol === 'http:' ? '/forest-full.github.io' : '';
+const isDevelopMode = ['localhost', '127.0.0.1'].includes(location.hostname);
 
 function isEmpty(firstTagContent) {
     return firstTagContent === null || firstTagContent === undefined;
-}
-
-function isMobile() {
-    const mobileAgent = ["iphone", "lgtelecom", "skt", "mobile", "samsung", "nokia", "blackberry", "android", "android", "sony", "phone"];
-    return mobileAgent.filter(agent => navigator.userAgent.indexOf(agent) !== -1).length > 0;
 }
 
 function getPage(uri) {
@@ -22,39 +18,11 @@ function getPage(uri) {
     xhr.open('GET', devUriPrefix + '/page' + uri.replaceAll('.html', '') + '.html', false);
     xhr.onreadystatechange = e => {
         if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-            history.pushState(pageSection.innerHTML, document.title, uri);
+            if (!isDevelopMode) history.pushState(pageSection.innerHTML, document.title, uri);
+
             pageSection.innerHTML = '';
             typing(pageSection, xhr.responseText, 10);
         }
     }
     xhr.send();
-}
-
-let timeoutAddress;
-const explainNav = document.querySelector('#explain-nav');
-
-function doubleClickCheck(object, callback) {
-    if (!isMobile()) return callback();
-
-    if (timeoutAddress !== undefined)
-        clearTimeout(timeoutAddress);
-
-    explainNav.innerHTML = '';
-
-    const classList = object?.classList;
-    if (classList?.contains('shine')) {
-        document.querySelectorAll('.shine')?.forEach(obj => obj?.classList.remove('shine'));
-        timeoutAddress = undefined;
-        callback();
-    } else {
-        document.querySelectorAll('.shine')?.forEach(obj => obj?.classList.remove('shine'));
-        classList?.add('shine');
-        typing(explainNav, object?.dataset?.explain, 10);
-        timeoutAddress = setTimeout(() => {
-            explainNav.innerHTML = '';
-            timeoutAddress = undefined;
-            if (classList?.contains('shine'))
-                classList?.remove('shine');
-        }, 10000);
-    }
 }
